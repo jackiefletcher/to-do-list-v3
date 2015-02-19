69a5d499f18e6477b92854d54cd7ff0b9154c4a0
Jackie Fletcher & Mike Harris <student@epicodus.com>
2015-02-19 15:28:18 -0800

    Add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,62 @@
+var List = {
+  name: "",
+}
+
 $(document).ready(function() {
-  $('form#to-do').submit(function(event) {
+  var currentList = null;
+
+  $('form#list').submit(function(event){
     event.preventDefault();
 
-    var inputTask = $('input#task').val();
-    var inputDueDate = $('input#due_date').val();
-    var inputDetails = $('input#details').val();
+    var inputName = $("input#name").val();
 
-    var newToDo = {task: inputTask, dueDate: inputDueDate, details: inputDetails};
+    var newList = Object.create(List);
 
-    $('ul#tasks').append("<li><span class='task '>" + newToDo.task + "</span></li>");
-    // $('ul#tasks').append("<button class='btn btn-sm btn-info' id='complete'>Completed?</button>");
-    $('input#task').val("");
-    $('input#due_date').val("");
-    $('input#details').val("");
+    newList.name = inputName;
+    newList.tasks = []
 
-    $(".task").last().click(function() {
-      $("#this-task").show();
-      $("#this-task h4").text(newToDo.task);
 
-      $(".Due_Date").text(newToDo.dueDate);
-      $(".Details").text(newToDo.details);
+
+    $('ul#lists').append("<li><span class='list'>" + newList.name + "</span></li>");
+
+    $('input#name').val("");
+
+    $(".list").last().click(function(){
+      $(".task-show").show();
+      $(".this-list").text(newList.name);
+      currentList = newList;
+      $('ul#tasks').text("");
+      currentList.tasks.forEach(function(task) {
+        $('ul#tasks').append("<li><span class='task'>" + task.taskName + "</span></li>");
+      });
     });
-
-    $("li").last().dblclick(function() {
-      $(this).appendTo("ul#completed");
-    });
-
-    // $("button#complete").click(function() {
-    //   $('#tasks li').appendTo("ul#completed");
-    //   $(this).remove();
-    // });
-
   });
+
+    $('form#to-do').submit(function(event) {
+      event.preventDefault();
+
+      var inputTask = $('input#task').val();
+      var inputDueDate = $('input#due_date').val();
+      var inputDetails = $('input#details').val();
+
+      var newToDo = {taskName: inputTask, dueDate: inputDueDate, details: inputDetails};
+        currentList.tasks.push(newToDo); //currentlist
+
+      $('ul#tasks').append("<span class = 'task'><li>" + newToDo.taskName + "</li></span>");
+
+      $('input#task').val("");
+      $('input#due_date').val("");
+      $('input#details').val("");
+
+      $(".task").last().click(function() {
+        $("#this-task").show();
+        $("#this-task h4").text(currentList.newTodo.taskName);
+        $(".Due_Date").text(currentList.newTodo.dueDate);
+        $(".Details").text(currentList.newTodo.details);
+      });
+
+      $(".task").last().dblclick(function() {
+        $(this).appendTo("ul#completed");
+      });
+    });
 });
